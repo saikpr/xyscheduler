8ff50ccae0dafa1a3b4af24dbe5baba2fec5445d
Sainyam Kapoor <sainyamkapoor@yahoo.com>
2014-10-24 23:06:03 +0530

    running job as threads

--- daemon.py
+++ daemon.py
@@ -1,7 +1,31 @@
 import json
+import threading
 from bottle import route, run, request, abort
 from xyvar import hostname,d_ID,c_ID
-from subprocess import Popen
+import subprocess 
+class jobThread(threading.Thread):
+     def __init__(self,jobargs,t_ID):
+        threading.Thread.__init__(self)
+        self.jobargs=jobargs
+        self.t_ID=t_ID
+
+     def run(self):
+        fileoutput=open(str(t_ID)+'.output','w')
+        job_popen = subprocess.Popen(jobargs
+                                    bufsize=-1,
+                                    executable=None,
+                                    stdin=None,
+                                    stdout=fileoutput,
+                                    stderr=subprocess.STDOUT, #check for deadlock Popen.communicate()
+                                    shell=False,
+                                    env=None,
+                                    universal_newlines=False,
+                                    startupinfo=None,
+                                    creationflags=0)
+        job_popen.wait()
+        ##call /daemon complete on master
+        fileoutput.close()
+         
 @route('/run', method='POST')
 def run_job():
     data = request.body.readline().decode('utf-8')
@@ -17,21 +41,15 @@ def run_job():
         abort(404, 'Wrong cluster?')
     t_ID=entity['t_ID'] #task id
     noofargs=entity['NumArgs']
-    jobarg =['/bin/sh','-c'] #check if safe?
-
+    jobargs =['/bin/sh','-c'] #check if safe?
+    
     for i in xrange(noofargs):
-        jobarg += entity['ARG-'+str(i)]
-    job_popen = Popen(jobarg,
-        bufsize=-1,
-        executable=None,
-        stdin=None,
-        stdout=None,
-        stderr=None,
-        shell=False,
-        env=None,
-        universal_newlines=False,
-        startupinfo=None,
-        creationflags=0)
+        jobargs += entity['ARG-'+str(i)]
+    jobT=jobThread(jobargs,t_ID)
+    jobT.start()
+
+    
+    
 
  
 @route('/rm/:id1', method='GET')
