fd79b5b532c14f7a9cd6bd0771213d936873ad53
Sainyam Kapoor <sainyamkapoor@yahoo.com>
2014-10-25 21:45:21 +0530

    updated xyvar with ips

--- xyvar.py
+++ xyvar.py
@@ -2,4 +2,6 @@ hostname="Some Name"
 d_ID="4324324234" #daemon_id
 c_ID="3423423423" #cluster id
 md_ID="234234324" #master daemon id
-slaves=["4324324234","4324324234","4324324234"] #all available daemon ids
+slaves={"4324324234":"10.0.0.34",
+		"4324324234":"10.0.0.23",
+		"4324324234":"10.0.0.25"} #all available daemon ids with corresponding ips
